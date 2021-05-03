148370bfb31c204506357d407b6dd8bea0142ea8
tl536 <tl536@scarletmail.rutgers.edu>
2021-05-03 08:19:54 -0400

    update

---
+++ Assets/Door1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door1 : MonoBehaviour
+{
+    Animator _doorAnim;
+    void Start()
+    {
+        _doorAnim = this.transform.parent.GetComponent<Animator>();
+    }
+    
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Hero")
+        {
+            _doorAnim.SetBool("IsOpening", true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Hero")
+        {
+            _doorAnim.SetBool("IsOpening", false);
+        }
+    }
+}
---
+++ Assets/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    
+    public Transform customPivot;
+    public GameObject player;
+
+
+    public void Update()
+    {
+        transform.RotateAround(customPivot.position, Vector3.up, 25 * Time.deltaTime);
+    }
+
+}
+
