9ee791551e634330a728efc8135015a5765fd818
tl536 <tl536@scarletmail.rutgers.edu>
2021-05-06 05:56:34 -0400

    up

---
+++ Assets/DetectPlayer1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectPlayer1 : MonoBehaviour
+{
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Hero"))
+        {
+            if (transform.childCount > 0)
+            {
+                this.transform.GetChild(0).gameObject.SetActive(true);
+            }
+
+
+        }
+    }
+}
